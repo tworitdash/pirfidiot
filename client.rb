15d40bc120d73f4ad0b48670e4556296ab46305f
tworitdash <tworitdash@gmail.com>
2015-10-11 15:10:08 +0000

    Testing Purposes

---
+++ client.rb
@@ -0,0 +1,59 @@
+#run this code in any system with a USB RFID connected. Tested in Mac OSX and Raspberry Pi. 
+#This client receives the data from the RFID reader and sends it to the server.
+
+
+require_relative 'em-websocket-client'
+require "serialport"
+require "json"
+
+#rfid object 
+#9600 is the baud rate for the RFID reader and 
+#"/dev/tty.usbserial-A90246LK" can be different fot your system. Make sure the port is assigned to the rfid reader.
+rfid = SerialPort.new("/dev/ttyUSB0", 9600)
+rfid.read_timeout = 1000
+
+EM.run do
+    #conn = EventMachine::WebSocketClient.connect("ws://web-tworit.rhcloud.com:8000/")
+    #if you want to accept the data from the rfid tags through a local server then uncomment the following line and make the above a comment line.
+    conn = EventMachine::WebSocketClient.connect("ws://192.168.43.252:8000/")
+    conn.callback do
+	msg = {"type" => "connection", "id" => "0", "name" => "RFID Tag"}.to_json
+        conn.send_msg msg
+       	#puts "connected !" 
+    end
+
+    conn.errback do |e|
+        puts "Got error: #{e}"
+    end
+
+    conn.stream do |msg|
+    	
+    	puts "received: #{msg}"
+    	
+    	tag = rfid.gets #receives data from reader. 
+        rfid_msg = {
+		"type" => "tag",
+		"id" => tag,
+		"name" => "LockID"
+	}
+	puts rfid_msg.to_json
+	
+            #puts "waiting for a card"
+	    File.open("public/temp.json", "w") do |f|
+		f.write(rfid_msg.to_json)
+	    end           
+            #puts tag #prints the tag ID
+    	    #puts tag.class #prints the tag data type
+            #puts tag.inspect #prints the exact data being received 
+            conn.send_msg rfid_msg.to_json #sends data to the websocket server
+        
+    			
+     end
+
+     
+
+    conn.disconnect do
+        puts "disconnected!"
+        EM::stop_event_loop
+    end
+end
---
+++ server.rb
@@ -0,0 +1,41 @@
+#server for the RFID data communication !
+
+#require 'thin'
+require 'em-websocket'
+#require 'sinatra/base'
+require 'json'
+
+EM.run do
+   
+    #client array
+
+    @clients = []
+
+    EM::WebSocket.start(:host => '0.0.0.0', :port => '8000') do |ws|
+        ws.onopen do |handshake|
+            @clients << ws
+	    puts @clients
+            ws.send "connected"
+            puts "Connected"
+        end
+
+        ws.onclose do
+            ws.send "Closed."
+            puts "closed"
+            @clients.delete ws
+        end
+
+        ws.onmessage do |msg|
+	   puts "Received Message: #{msg}"
+	   ser = msg.to_json
+	   puts tag = JSON.parse(ser, :quirks_mode => true)
+	   puts msg.class
+
+            #The data collected from the server is then sent to each of the connected clients to the server. 
+            @clients.each do |socket|
+                socket.send msg
+            end
+        end
+    end
+
+end
